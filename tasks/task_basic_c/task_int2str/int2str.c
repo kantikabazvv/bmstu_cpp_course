f400721d99463024345b71ac5d5afb53a6b096d2
kanti@kanti <kantikabazov@gmail.com>
2024-10-19 00:36:14 +0300

    Программа проходит все тесты

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,32 +1,35 @@
 #include <assert.h>
 #include "int2str.h"
 #include "stdio.h"
-#include <string.h>
+#include <malloc.h>
 
-char * int2str(int number) {
-    if (number == 0) return "0";
-    else if (number == -2147483648) return "-2147483648"; 
-    else if (number == 2147483647) return "2147483647";
-    else {
-        char str [12];
-        int i = 0;
-        int sign = (number >= 0 ? 1 : -1);
-        number *= sign;
-        while (number > 0){
-            str[i++] = number % 10 + '0';
-            number /= 10;
-        }
-        if (sign < 0) str[i++] = '-';
-        str[i] = '\0';
-        for (int j = 0, k = i - 1; j < k; j++, k--){
-            char c = str[j];
-            str[j] = str[k];
-            str[k] = c;
-        }
-        char * const res = (char *)malloc(i*sizeof(char *));
-        for (int m = 0; m <= i; m++){
-            res[m] = str[m];
-        }
-        return res;
+char* int2str(int number) {
+    int i = 0;
+    int isNegative = 0;
+    char *str = "0";
+    str = (char*)malloc(20 * sizeof(char));
+
+    if (number < 0) {
+        isNegative = 1;
+        number = -number;
     }
+
+    do {
+        str[i++] = (number % 10) + '0';  
+        number /= 10;                       
+    } while (number > 0);
+
+    if (isNegative==1) {
+        str[i++] = '-';                     
+    }
+
+    str[i] = '\0';                         
+
+    for (int j = 0; j < i / 2; j++) {
+        char temp = str[j];
+        str[j] = str[i - j - 1];
+        str[i - j - 1] = temp;
+    }
+    
+    return str;
 }
